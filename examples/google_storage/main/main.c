e7ba60549c739f4cc6c52f3f26f89d815ac217ef
Raphael Brandão <raphael.b.souza@hotmail.com>
2019-10-08 02:53:21 -0300

    testing

--- examples/google_storage/main/main.c
+++ examples/google_storage/main/main.c
@@ -1,26 +1,35 @@
-#include <esp_wifi.h>
-#include <esp_event_loop.h>
-#include <esp_log.h>
 #include <time.h>
 #include <sys/time.h>
 #include <esp_system.h>
-#include <nvs_flash.h>
 #include <sys/param.h>
-#include <freertos/FreeRTOS.h>
-#include <freertos/event_groups.h>
-#include <esp_http_client.h>
-#include <string.h>
 #include "lwip/err.h"
 #include "lwip/apps/sntp.h"
+
+#include <stdio.h>
+#include <string.h>
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <freertos/event_groups.h>
+
+#include <esp_log.h>
+#include <esp_wifi.h>
+#include <esp_event_loop.h>
+#include <nvs_flash.h>
+
+#include <wifi_provisioning/manager.h>
+#include <wifi_provisioning/scheme_ble.h>
+#include <wifi_provisioning/scheme_softap.h>
 
 #include "esp_camera.h"
 #include "google_api.h"
 
-#define WIFI_CONNECTED BIT0
+#define WIFI_CONNECTED_EVENT BIT0
 #define SYNC_NTP BIT1
 #define HAS_AUTH_TOKEN BIT2
 
 static EventGroupHandle_t eventGroup;
+static EventGroupHandle_t wifi_event_group;
 
 static camera_config_t camera_config = {
     .pin_pwdn = -1,
@@ -96,10 +105,10 @@ static void task_gcp()
   heap_caps_print_heap_info(MALLOC_CAP_8BIT);
   while (1)
   {
-    EventBits_t bit = xEventGroupWaitBits(eventGroup, (WIFI_CONNECTED | SYNC_NTP | HAS_AUTH_TOKEN), false, false, portMAX_DELAY);
+    EventBits_t bit = xEventGroupWaitBits(eventGroup, (WIFI_CONNECTED_EVENT | SYNC_NTP | HAS_AUTH_TOKEN), false, false, portMAX_DELAY);
     esp_err_t err;
 
-    if (bit == WIFI_CONNECTED)
+    if (bit == WIFI_CONNECTED_EVENT)
     {
       initialize_sntp();
       struct tm timeinfo = {0};
@@ -117,7 +126,7 @@ static void task_gcp()
       ESP_LOGI(TAG, "Set SYNC_NTP");
       xEventGroupSetBits(eventGroup, SYNC_NTP);
     }
-    else if (bit == (WIFI_CONNECTED | SYNC_NTP))
+    else if (bit == (WIFI_CONNECTED_EVENT | SYNC_NTP))
     {
       if (gcp_auth_refresh_token() == ESP_OK && gcp_check_bucket_access() == ESP_OK)
       {
@@ -129,58 +138,131 @@ static void task_gcp()
         clear_auth_token();
       }
     }
-    else if (bit == (WIFI_CONNECTED | SYNC_NTP | HAS_AUTH_TOKEN))
+    else if (bit == (WIFI_CONNECTED_EVENT | SYNC_NTP | HAS_AUTH_TOKEN))
     {
       take_picture_and_upload_to_google_storage();
     }
 
     heap_caps_print_heap_info(MALLOC_CAP_8BIT);
-    vTaskDelay(20000 / portTICK_RATE_MS);
+    vTaskDelay(10000 / portTICK_RATE_MS);
   }
 }
 
 static esp_err_t wifi_event_handler(void *ctx, system_event_t *event)
 {
-  switch (event->event_id)
-  {
-  case SYSTEM_EVENT_STA_START:
-    ESP_LOGI(TAG, "SYSTEM_EVENT_STA_START");
-    ESP_ERROR_CHECK(esp_wifi_connect());
-    break;
-  case SYSTEM_EVENT_STA_GOT_IP:
-    ESP_LOGI(TAG, "SYSTEM_EVENT_STA_GOT_IP");
-    ESP_LOGI(TAG, "Got IP: '%s'",
-             ip4addr_ntoa(&event->event_info.got_ip.ip_info.ip));
-    xEventGroupSetBits(eventGroup, WIFI_CONNECTED);
-    break;
-  case SYSTEM_EVENT_STA_DISCONNECTED:
-    ESP_LOGI(TAG, "SYSTEM_EVENT_STA_DISCONNECTED");
-    ESP_ERROR_CHECK(esp_wifi_connect());
-    xEventGroupClearBits(eventGroup, WIFI_CONNECTED);
-    break;
-  default:
-    break;
+  /* Pass event information to provisioning manager so that it can
+    * maintain its internal state depending upon the system event */
+  wifi_prov_mgr_event_handler(ctx, event);
+
+  /* Global event handling */
+  switch (event->event_id) {
+    case SYSTEM_EVENT_STA_START:
+      esp_wifi_connect();
+      break;
+    case SYSTEM_EVENT_STA_GOT_IP:
+      ESP_LOGI(TAG, "Connected with IP Address:%s",
+                ip4addr_ntoa(&event->event_info.got_ip.ip_info.ip));
+      xEventGroupSetBits(wifi_event_group, WIFI_CONNECTED_EVENT);
+      break;
+    case SYSTEM_EVENT_STA_DISCONNECTED:
+      ESP_LOGI(TAG, "Disconnected. Connecting to the AP again...");
+      esp_wifi_connect();
+      break;
+    default:
+      break;
   }
   return ESP_OK;
+}
+
+static void prov_event_handler(void *user_data,
+                               wifi_prov_cb_event_t event, void *event_data)
+{
+  switch (event) {
+    case WIFI_PROV_START:
+      ESP_LOGI(TAG, "Provisioning started");
+      break;
+    case WIFI_PROV_CRED_RECV: {
+      wifi_sta_config_t *wifi_sta_cfg = (wifi_sta_config_t *)event_data;
+      /* If SSID length is exactly 32 bytes, null termination
+        * will not be present, so explicitly obtain the length */
+      size_t ssid_len = strnlen((const char *)wifi_sta_cfg->ssid, sizeof(wifi_sta_cfg->ssid));
+      ESP_LOGI(TAG, "Received Wi-Fi credentials"
+                "\n\tSSID     : %.*s\n\tPassword : %s",
+                ssid_len, (const char *) wifi_sta_cfg->ssid,
+                (const char *) wifi_sta_cfg->password);
+      break;
+    }
+    case WIFI_PROV_CRED_FAIL: {
+      wifi_prov_sta_fail_reason_t *reason = (wifi_prov_sta_fail_reason_t *)event_data;
+      ESP_LOGE(TAG, "Provisioning failed!\n\tReason : %s"
+                "\n\tPlease reset to factory and retry provisioning",
+                (*reason == WIFI_PROV_STA_AUTH_ERROR) ?
+                "Wi-Fi AP password incorrect" : "Wi-Fi AP not found");
+      break;
+    }
+    case WIFI_PROV_CRED_SUCCESS:
+      ESP_LOGI(TAG, "Provisioning successful");
+      break;
+    case WIFI_PROV_END:
+      /* De-initialize manager once provisioning is finished */
+      wifi_prov_mgr_deinit();
+      break;
+    default:
+      break;
+  }
 }
 
 static void initialise_wifi()
 {
   tcpip_adapter_init();
   ESP_ERROR_CHECK(esp_event_loop_init(wifi_event_handler, NULL));
+  wifi_event_group = xEventGroupCreate();
+
   wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
   ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-  ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
-  wifi_config_t wifi_config = {
-      .sta = {
-          .ssid = CONFIG_WIFI_SSID,
-          .password = CONFIG_WIFI_PASSWORD,
-      },
+
+  wifi_prov_mgr_config_t config = {
+      .scheme = wifi_prov_scheme_ble,
+      .scheme_event_handler = WIFI_PROV_SCHEME_BLE_EVENT_HANDLER_FREE_BTDM,
+      .app_event_handler = {
+          .event_cb = prov_event_handler,
+          .user_data = NULL
+      }
   };
-  ESP_LOGI(TAG, "Setting WiFi configuration SSID %s...", wifi_config.sta.ssid);
-  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-  ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
-  ESP_ERROR_CHECK(esp_wifi_start());
+
+  ESP_ERROR_CHECK(wifi_prov_mgr_init(config));
+
+  bool provisioned = false;
+  ESP_ERROR_CHECK(wifi_prov_mgr_is_provisioned(&provisioned));
+
+  if (!provisioned) {
+    ESP_LOGI(TAG, "Starting provisioning");
+
+    wifi_prov_security_t security = WIFI_PROV_SECURITY_1;
+    const char *pop = "rapha123";
+
+    uint8_t custom_service_uuid[] = {
+        /* LSB <---------------------------------------
+          * ---------------------------------------> MSB */
+        0x21, 0x43, 0x65, 0x87, 0x09, 0xba, 0xdc, 0xfe,
+        0xef, 0xcd, 0xab, 0x90, 0x78, 0x56, 0x34, 0x12
+    };
+    wifi_prov_scheme_ble_set_service_uuid(custom_service_uuid);
+
+    /* Start provisioning service */
+    ESP_ERROR_CHECK(wifi_prov_mgr_start_provisioning(security, pop, "RAPHA_BRANDAO", NULL));
+  } else {
+    ESP_LOGI(TAG, "Already provisioned, starting Wi-Fi STA");
+
+    /* We don't need the manager as device is already provisioned,
+      * so let's release it's resources */
+    wifi_prov_mgr_deinit();
+
+    /* Start Wi-Fi station */
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_start());
+  }
+  xEventGroupWaitBits(wifi_event_group, WIFI_CONNECTED_EVENT, false, true, portMAX_DELAY);
 }
 
 void app_main()

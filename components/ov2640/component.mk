f88fc9437b5cd75736c0adc785605506a8fee514
Raphael Brandão <raphael.b.souza@hotmail.com>
2020-07-16 21:09:22 -0300

    remove local components

--- components/ov2640/component.mk
+++
@@ -1,4 +0,0 @@
-COMPONENT_ADD_INCLUDEDIRS := conversions/include camera/include
-COMPONENT_PRIV_INCLUDEDIRS := conversions/private_include camera/private_include sensors/private_include
-COMPONENT_SRCDIRS := conversions camera sensors
-CXXFLAGS += -fno-rtti
